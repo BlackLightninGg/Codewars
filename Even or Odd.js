91fbf53ba59ec86a4572824f3e591d2f4d937973
Aleksey <easy.mail.to.copy@gmail.com>
2023-03-16 23:26:16 +0300

    Solving kata "Even or Odd"

--- Even or Odd.js
+++ Even or Odd.js
@@ -1,5 +1,10 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.//
 
 function evenOrOdd(number) {
-
+ return number % 2 === 0 ? "Even" : "Odd";
 }
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(11));
+console.log(evenOrOdd(-46));
+console.log(evenOrOdd(-9));
