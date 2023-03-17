e56eb316b296ab30b464d40b54dde6e9d3571004
Aleksey <easy.mail.to.copy@gmail.com>
2023-03-17 23:24:03 +0300

    Solving kata "Credit Card Mask" and beginning kata "Exes and Ohs"

--- Credit Card Mask.js
+++ Credit Card Mask.js
@@ -1,4 +1,18 @@
-// Your task is to write a function maskify, which changes all but the last four characters into '#'.
 function maskify(cc) {
+    b = [...cc]
+  
+      if (b.length > 4) {
+          for (let i = 0; i < b.length - 4; i++) {
+              b[i] = "#";
+          }
+      } else {
+         return b.join("");
+      }
+      return b.join("");
+  }
 
-}
+console.log(maskify("afasr"));
+console.log(maskify("a356fas1234rhg59"));
+console.log(maskify("a356fas1234rhg59%$$"));
+console.log(maskify(")!"));
+console.log(maskify("1"));
