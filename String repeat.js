dcc9e6875374bb37e43bb51a0b9cea1c1a6d4623
Aleksey <easy.mail.to.copy@gmail.com>
2023-03-19 15:25:25 +0300

    Solved kata "String repeat" and started kata "Simple multiplication".

--- String repeat.js
+++ String repeat.js
@@ -1,4 +1,13 @@
 //Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.//
-function repeatStr (n, s) {
-    return '';
+function repeatStr(n, s) {
+    let b
+    let a = ""
+    for (let i = 0; i < n; i++) {
+        a += s;
+        b = a;
+    }
+    return b;
 }
+
+console.log(repeatStr(9, "Hello"));
+console.log(repeatStr(2, "a"));
