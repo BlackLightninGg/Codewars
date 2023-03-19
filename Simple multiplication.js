f3e27b4755191988d9a8b8d3a6475828234a9407
Aleksey <easy.mail.to.copy@gmail.com>
2023-03-19 20:06:07 +0300

    Solved kata "Simple multiplication" and started kata "Find The Parity Outlier".

--- Simple multiplication.js
+++ Simple multiplication.js
@@ -1,5 +1,11 @@
 /*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.*/
 
-    function simpleMultiplication(number) {
-
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? (number * 8) : (number * 9)
 }
+
+console.log(simpleMultiplication(2));
+console.log(simpleMultiplication(51));
+console.log(simpleMultiplication(98));
+console.log(simpleMultiplication(-2));
+console.log(simpleMultiplication(0));
